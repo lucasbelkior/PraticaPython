75ef844fcbaa7c958156e07231c297094ff1c12f
Lucas Belkior <lucasbelkior9@gmail.com>
2023-08-01 11:00:19 -0300

    Initial commit

---
+++ ex_fixacao01.py
@@ -0,0 +1,7 @@
+nome = input("Qual é o seu nome?")
+print("Ola,",nome,"tudo bem com voce?")
+num = float(input("Informe um valor em dolar:"))
+conver = num * 4.78
+
+print("Considere que 1 dolar vale 4.78 reais")
+print("O seu valor em reais será:",conver,"reais")
