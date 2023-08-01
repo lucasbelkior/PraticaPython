3e7b1848b77f264f7a9637fb1197cd9e361a8687
Lucas Belkior <lucasbelkior9@gmail.com>
2023-08-01 11:22:59 -0300

    2 commit

---
+++ ex_fixacao02.py
@@ -0,0 +1,6 @@
+nome = input("Qual é o seu nome?")
+print("Ola,",nome,"tudo bem com voce?")
+val = float(input("Qual o valor a ser pago ao restaurante?:"))
+cont = (val*10/100)+val
+print("O valor a ser pago é", val ,"com um acrescimo de 10% do garçom,Senhor" )
+print("Portanto,o valor a ser pago é",cont,"reais")
